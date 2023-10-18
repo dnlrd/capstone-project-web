3776d0758e322503a763c7f0db75b5ae54a9d985
dnlrd <deanjaylord.dnlrd@gmail.com>
2023-10-18 17:26:18 +0800

    Update household-detail.blade.php

--- resources/views/pages/records/household-detail.blade.php
+++ resources/views/pages/records/household-detail.blade.php
@@ -1,89 +1,150 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-table, th, td {
-  border:1px solid black;
-}
-</style>
-        <h1>Household Details</h1>
-        <table>
-    <thead>
-        <tr>
-            <th>Household Code</th>
-            <th>Year</th>
-            <th>Tag No</th>
-            <th>Barangay</th>
-            <th>Purok</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Last Name</th>
-        </tr>
-    </thead>
-    <tbody>
-            <tr>
-                <td>{{ $household->household_code }}</td>
-                <td>{{ $household->year }}</td>
-                <td>{{ $household->tag_no }}</td>
-                <td>{{ $household->barangay }}</td>
-                <td>{{ $household->purok }}</td>
-                <td>{{ $household->firstname }}</td>
-                <td>{{ $household->middlename }}</td>
-                <td>{{ $household->lastname }}</td>
-            </tr>
-    </tbody>
-</table>
+<div class="page-header d-print-none text-black">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center mb-3">
+            <div class="col">
+                <h2 class="page-title">
+                    Household Details
+                </h2>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Household Information of {{ $household->household_code }}</h3>
+            </div>
+            <div class="card-body">
+                <table class="table card-table table-vcenter text-nowrap datatable">
+                    <thead>
+                        <tr>
+                            <th>Household Code</th>
+                            <th>Year</th>
+                            <th>Tag No</th>
+                            <th>Barangay</th>
+                            <th>Purok</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <tr>
+                                <td>{{ $household->household_code }}</td>
+                                <td>{{ $household->year }}</td>
+                                <td>{{ $household->tag_no }}</td>
+                                <td>{{ $household->barangay }}</td>
+                                <td>{{ $household->purok }}</td>
+                            </tr>
+                    </tbody>
+                </table>
+                <div class="row mb-3">
+                    <div class="card col-md-6">
+                        <div class="card-body">
+                            <h3 class="card-title">1. Pangalan ng Puno ng Pamilya:</h3>
+                            <div class="row row-cards">
+                                <div class="col-sm-12 col-md-4">
+                                    <div class="form-group">
+                                        <label for="pangalan">Pangalan</label>
+                                        <div class="fw-bold">{{ $household->firstname }}</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <div class="form-group">
+                                        <label for="gitnang_pangalan">Gitnang Pangalan</label>
+                                        <div class="fw-bold">{{ $household->middlename }}</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <div class="form-group">
+                                        <label for="apelyido">Apelyido</label>
+                                        <div class="fw-bold">{{ $household->lastname }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-md-6">
+                        <div class="card-body">
+                            <h3 class="card-title">2a. Kasalukuyang Tirahan</h3>
+                            <div class="row row-cards">
+                                <div class="col-sm-12 col-md-9">
+                                    <div class="form-group">
+                                        <label>(Bilang at Kalye)</label>
+                                        <div class="fw-bold">{{$question2->answer1_q2}}</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-md-3 mb-3">
+                                    <div class="form-group">
+                                        <label>(Barangay)</label>
+                                        <div class="fw-bold">{{$question2->answer2_q2}}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row row-cards">
+                                <div class="col-sm-12 col-md-12">
+                                    <h3 class="card-title">2b. Taon ng Paglipat</h3>
+                                    <div class="form-group">
+                                        <label>Taon</label>
+                                        <div class="fw-bold">{{$question2->answer3_q2}}</div>
+                                    </div>
+                                </div>
+                            </div>      
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
 
         <table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Birthdate</th>
-            <th>Relationship to Head</th>
-            <th>Gender</th>
-            <th>Age</th>
-            <th>Civil Status</th>
-            <th>Solo Parent</th>
-            <th>Religion</th>
-            <th>Studying</th>
-            <th>Has Job</th>
-            <th>Job</th>
-            <th>Known Work</th>
-            <th>Where</th>
-            <th>Sector</th>
-            <th>Position</th>
-            <th>Monthly Income</th>
-            <th>Level of Nutrition</th>
-            <th>Type of Disability</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($familyMembers as $familyMember)
-            <tr>
-                <td>{{ $familyMember->lastname }}, {{ $familyMember->firstname }} {{ $familyMember->middlename }}</td>
-                <td>{{ $familyMember->birthdate }}</td>
-                <td>{{ $familyMember->relationship_to_head }}</td>
-                <td>{{ $familyMember->gender }}</td>
-                <td>{{ $familyMember->age }}</td>
-                <td>{{ $familyMember->civil_status }}</td>
-                <td>{{ $familyMember->solo_parent }}</td>
-                <td>{{ $familyMember->religion }}</td>
-                <td>{{ $familyMember->studying }}</td>
-                <td>{{ $familyMember->has_job }}</td>
-                <td>{{ $familyMember->job }}</td>
-                <td>{{ $familyMember->known_work }}</td>
-                <td>{{ $familyMember->where }}</td>
-                <td>{{ $familyMember->sector }}</td>
-                <td>{{ $familyMember->position }}</td>
-                <td>{{ $familyMember->monthly_income }}</td>
-                <td>{{ $familyMember->level_of_nutrition }}</td>
-                <td>{{ $familyMember->type_of_disability }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Birthdate</th>
+                    <th>Relationship to Head</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                    <th>Civil Status</th>
+                    <th>Solo Parent</th>
+                    <th>Religion</th>
+                    <th>Studying</th>
+                    <th>Has Job</th>
+                    <th>Job</th>
+                    <th>Known Work</th>
+                    <th>Where</th>
+                    <th>Sector</th>
+                    <th>Position</th>
+                    <th>Monthly Income</th>
+                    <th>Level of Nutrition</th>
+                    <th>Type of Disability</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($familyMembers as $familyMember)
+                    <tr>
+                        <td>{{ $familyMember->lastname }}, {{ $familyMember->firstname }} {{ $familyMember->middlename }}</td>
+                        <td>{{ $familyMember->birthdate }}</td>
+                        <td>{{ $familyMember->relationship_to_head }}</td>
+                        <td>{{ $familyMember->gender }}</td>
+                        <td>{{ $familyMember->age }}</td>
+                        <td>{{ $familyMember->civil_status }}</td>
+                        <td>{{ $familyMember->solo_parent }}</td>
+                        <td>{{ $familyMember->religion }}</td>
+                        <td>{{ $familyMember->studying }}</td>
+                        <td>{{ $familyMember->has_job }}</td>
+                        <td>{{ $familyMember->job }}</td>
+                        <td>{{ $familyMember->known_work }}</td>
+                        <td>{{ $familyMember->where }}</td>
+                        <td>{{ $familyMember->sector }}</td>
+                        <td>{{ $familyMember->position }}</td>
+                        <td>{{ $familyMember->monthly_income }}</td>
+                        <td>{{ $familyMember->level_of_nutrition }}</td>
+                        <td>{{ $familyMember->type_of_disability }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-{{$question2->answer1_q2}}<br>
+<br>
 {{$question2->answer2_q2}}<br>
 {{$question2->answer3_q2}}<br>
 <br>
@@ -101,6 +162,22 @@ table, th, td {
 {{$question6->answer1_q6}}<br>
 <br>
 {{$familyMemberCount}}
+<br>
+@foreach ($question9 as $data)
+    <p>if_yes: {{ $data->if_yes }}</p>
+    <p>answer1_q9: {{ $data->answer1_q9 }}</p>
+    <p>answer2_q9: {{ $data->answer2_q9 }}</p>
+    <p>answer3_q9: {{ $data->answer3_q9 }}</p>
+    <p>answer4_q9: {{ $data->answer4_q9 }}</p>
+    <p>answer5_q9: {{ $data->answer5_q9 }}</p>
+@endforeach
+<br>
+@foreach ($question10 as $data)
+    <p>answer1_q10: {{ $data->answer1_q10 }}</p>
+    <p>answer2_q10: {{ $data->answer2_q10 }}</p>
+    <p>answer3_q10: {{ $data->answer3_q10 }}</p>
+    <p>answer4_q10: {{ $data->answer4_q10 }}</p>
+@endforeach
 <br>
 {{$question11->answer1_q11}}<br>
 {{$question11->answer2_q11}}<br>
@@ -124,20 +201,8 @@ table, th, td {
 {{$question17->answer1_q17}}<br>
 {{$question17->answer2_q17}}<br>
 {{$question17->answer3_q17}}<br>
-<br>
-@foreach ($question9 as $data)
-    <p>if_yes: {{ $data->if_yes }}</p>
-    <p>answer1_q9: {{ $data->answer1_q9 }}</p>
-    <p>answer2_q9: {{ $data->answer2_q9 }}</p>
-    <p>answer3_q9: {{ $data->answer3_q9 }}</p>
-    <p>answer4_q9: {{ $data->answer4_q9 }}</p>
-    <p>answer5_q9: {{ $data->answer5_q9 }}</p>
-@endforeach
-<br>
-@foreach ($question10 as $data)
-    <p>answer1_q10: {{ $data->answer1_q10 }}</p>
-    <p>answer2_q10: {{ $data->answer2_q10 }}</p>
-    <p>answer3_q10: {{ $data->answer3_q10 }}</p>
-    <p>answer4_q10: {{ $data->answer4_q10 }}</p>
-@endforeach
+
+
+    </div>
+</div>
 @endsection
